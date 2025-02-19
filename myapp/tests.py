1cf865fc2289a50c7f5f36dd510f2dca03b3ac04
wangtianyu <a154844605@hotmail.com>
2025-02-19 16:23:25 +0800

    Revert "wty测试"
    This reverts commit 52f3e2c364b8801672d0d180b67c7b45296edab8.

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,11 +1,3 @@
 from django.test import TestCase
 
-class SampleTestCase(TestCase):
-    def test_example(self):
-        # 这是一个简单的测试用例
-        self.assertEqual(1 + 1, 2)
-
-    def test_home_page(self):
-        # 测试首页是否可以访问
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+# Create your tests here.
