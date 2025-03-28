bff661852141f2b7dab9efcce5d8e27eb1ba8b9f
Chen MJ <1343256265@qq.com>
2025-03-28 20:28:27 +0800

    项目管理修改-2

--- hotel/urls.py
+++ hotel/urls.py
@@ -117,6 +117,16 @@ urlpatterns = [
     path('train_model_board/', views.train_model_board),
     path('train_model_boardnew/', views.train_model_boardnew),
 
+    #项目管理
+    path('project_notarization/', views.project_notarization),
+    path('pengding_project/', views.pengding_project),
+    path('project_notarization_add/', views.project_notarization_add),
+    # path('datasharing_add/', views.datasharing_add),
+    # 返回所有存证信息
+    path('search_notarization/', views.search_notarization),
+    # 返回按项目名称查询存证信息
+    path('search_notarization_by_projectname/', views.search_notarization_by_projectname),
+
     #模型应用
     path('model-application/', modelappview.model_application),
     path('model-application_search/', modelappview.searchModelApplication),
@@ -220,4 +230,16 @@ urlpatterns = [
 
     path('multmodel_application_result_analysis/', viewsmult.multmodel_application_result_analysis),
     path('multmodel_application_status_modify/', viewsmult.editMultModelApplicationStatus),
+
+    # 项目管理
+    # 项目管理
+    path('established_project/', views.established_project),
+    path('pengding_project/', views.pending_project),
+    path('project_add/', views.project_add),
+    path('create_project/', views.create_project),
+
+    path('submit_project/', views.submit_project),
+    path('get_project_data/', views.get_project_data),
+    path('delete_project/', views.delete_project),
+    path('update_project/', views.update_project),
 ]
